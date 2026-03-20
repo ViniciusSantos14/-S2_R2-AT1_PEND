479ddae42e69841beb24cba230d11dbd0cd507c7
VINÍCIUS PAES DOS SANTOS <vinicius.santos432@senaisp.edu.br>
2026-03-20 09:39:28 -0300

    Atividade 2 atualizada

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-let item = document.querySelector("#item")
-let botaoAdicionar = document.querySelector("#botaoAdicionar")
-let listaItens = document.getElementById("listaItens")
-let mensagem = document.getElementById("mensagem")
+const botao = document.getElementById("btn");
+const texto = document.getElementById("texto");
 
-botaoAdicionar.addEventListener("click", () => {
-
-    let texto = item.value.trim()
-
-    if (texto === "") {
-
-        mensagem.innerHTML = '<div class="alert alert-danger">Tarefa vazia!</div>' // 🔸 NOVO
-
-    } else {
-
-        let novoItem = document.createElement("li")
-        novoItem.innerText = texto
-        novoItem.className = "list-group-item"
-
-        listaItens.appendChild(novoItem)
-
-        item.value = ""
-
-        mensagem.innerHTML = '<div class="alert alert-success">Tarefa adicionada!</div>' // 🔸 NOVO
-    }
-
-})
+botao.addEventListener("click", () => {
+  texto.innerText = "Texto alterado com JavaScript!";
+});
